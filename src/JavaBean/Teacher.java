52f1a294c41e27cf2da76cf7c39568f132c3ae70
pyq-system <123456@qq.com>
2020-05-14 14:47:05 +0800

    teacher

--- src/JavaBean/Teacher.java
+++ src/JavaBean/Teacher.java
@@ -1,5 +1,26 @@
 package JavaBean;
 
 public class Teacher {
-
+	private String teacherName;
+	private int tno;
+	private String course;
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public int getTno() {
+		return tno;
+	}
+	public void setTno(int tno) {
+		this.tno = tno;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
 }
